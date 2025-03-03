558b135ad65c0cbe98d0574e1cb6b5fbaf315d73
raidatulzukira <raidatulzukiraa@gmail.ac.id>
2025-03-03 12:08:45 +0700

    update

--- android/app/src/main/kotlin/com/zukira/latihanflutter1/MainActivity.kt
+++ android/app/src/main/kotlin/com/zukira/latihanflutter1/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.zukira.latihanflutter1
 
-import io.flutter.embedding.android.FlutterActivity
-
 class MainActivity : FlutterActivity()
+
+open class FlutterActivity {
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:latihanflutter1/Screen/page_column.dart';
+import 'package:latihanflutter1/Screen/page_dua.dart';
+import 'package:latihanflutter1/Screen/page_gambar.dart';
+import 'package:latihanflutter1/Screen/page_list_horizontal.dart';
+import 'package:latihanflutter1/Screen/page_row.dart';
+import 'package:latihanflutter1/Screen/page_row_column.dart';
+import 'package:latihanflutter1/Screen/page_satu.dart';
+import 'package:latihanflutter1/Screen/page_tiga.dart';
+import 'package:latihanflutter1/Screen/page_url_image.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -36,6 +46,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 //ketik st
 class PageUtama extends StatelessWidget {
   const PageUtama({super.key});
@@ -44,13 +55,85 @@ class PageUtama extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ZUKIRA APP MI 2A'),
-        backgroundColor: Colors.pinkAccent,
-        centerTitle: true,
+        title: Text('App MI 2A'),
+        backgroundColor: Colors.purple,
       ),
 
       body: Center(
-        child: Text('Welcome to MI 2A Apps'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Welcome to MI 2A Apps'),
+
+            MaterialButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)
+              => PageColom()
+              ));
+            },
+                color : Colors.orange,
+                child : Text ('Page Column', style: TextStyle(fontSize: 14, color: Colors.white))
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)
+                => PageRow()
+                ));
+              },
+                  elevation: 18.0,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                  color : Colors.orange,
+                  clipBehavior: Clip.antiAlias,
+                  child : Text ('Page Row', style: TextStyle(fontSize: 14, color: Colors.white))
+              ),
+            ),
+            MaterialButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)
+              => PageTiga()
+              ));
+            },
+                color : Colors.orange,
+                child : Text ('Page3', style: TextStyle(fontSize: 14, color: Colors.white))
+            ),
+            MaterialButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)
+              => PageRowColumn()
+              ));
+            },
+                color : Colors.orange,
+                child : Text ('Page Row Column', style: TextStyle(fontSize: 14, color: Colors.white))
+            ),
+
+            MaterialButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)
+              => PageGambar()
+              ));
+            },
+                color : Colors.pinkAccent,
+                child : Text ('Page Gambar', style: TextStyle(fontSize: 14, color: Colors.white))
+            ),
+
+            MaterialButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)
+              => PageListHorizontal()
+              ));
+            },
+                color : Colors.pinkAccent,
+                child : Text ('Page List Horizontal', style: TextStyle(fontSize: 14, color: Colors.white))
+            ),
+
+            MaterialButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)
+              => PageUrlImage()
+              ));
+            },
+                color : Colors.pinkAccent,
+                child : Text ('Page Url Image', style: TextStyle(fontSize: 14, color: Colors.white))
+            ),
+
+          ],
+        ),
       ),
     );
   }
